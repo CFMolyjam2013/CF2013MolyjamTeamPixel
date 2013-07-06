db79f7dfa81a4ac2c7ee8d2f921ce4e154b353f7
dre38w <dre38w@gmail.com>
2013-07-05 21:50:18 -0400

    Player code implimented

--- Assets/Scripts/Player/PlayerPhysics.cs
+++ Assets/Scripts/Player/PlayerPhysics.cs
@@ -3,53 +3,120 @@ using System.Collections;
 
 public class PlayerPhysics : MonoBehaviour
 {
-    public float playerSpeed = 10.0f;
+    //speeds of player in zombie states
+    public float fullHumanSpeed = 10.0f;
+    public float partHumanSpeed = 20.0f;
+    public float halfZombieSpeed = 30.0f;
+    public float nearFullZombieSpeed = 40.0f;
+    public float fullZombieSpeed = 60.0f;
 
+    public float playerRotSpeed = 120.0f;
+
+    //gid sizes
     public int xGridSize = 4;
     public int yGridSize = 4;
 
     private int frameDir = 0;
-    public int currentFrame = 0;
+    private int currentFrame = 0;
 
+    //position of the grid tiles
     private float xGridPos = 0.0f;
     private float yGridPos = 0.0f;
 
     private float frameDur = 0.0f;
     private float nextTimeFrame = 0.0f;
 
-    // Use this for initialization
-	void Start ()
+    //zombie levels
+    public enum ZombieState
     {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        fullHuman,
+        partHuman,
+        halfZombie,
+        nearFullZombie,
+        fullZombie
+    }
+
+    public ZombieState zombieStates { get; set; }
+
+    // Update is called once per frame
+    void Update()
     {
+        //TEMPORARY JUST FOR TESTING.  WILL REPLACE WITH COLLIDING WITH THE AI'S
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            zombieStates++;
+        }
+
         GetMotion();
-	}
+    }
 
     void GetMotion()
     {
-        float input = Input.GetAxisRaw("Vertical") * playerSpeed * Time.deltaTime;
+        //get input
+        float vInput = Input.GetAxisRaw("Vertical");
+        float hInput = Input.GetAxisRaw("Horizontal");
 
-        if (input != 0)
-        {
-            frameDir = (int)input;
-        }
-
+        //set grid tiles
         xGridPos = 1.0f / xGridSize;
         yGridPos = 0.5f;
 
-        renderer.material.mainTextureScale = new Vector2(frameDir * xGridPos, yGridPos);
+        //set scale
+        renderer.material.mainTextureScale = new Vector2(vInput * xGridPos, yGridPos);
 
         if (Time.time - nextTimeFrame > frameDur)
         {
-            nextTimeFrame = frameDur + Time.time;
+            nextTimeFrame = Time.time + frameDur;
 
-            currentFrame += frameDir;
+            if (vInput > 0)
+            {
+                currentFrame++;
+            }
+            if (vInput < 0)
+            {
+                currentFrame--;
+            }
 
-            renderer.material.mainTextureOffset = new Vector2(frameDir * ((currentFrame) % xGridSize + 1) * xGridPos, 1);
+            //loop frames
+            if (currentFrame >= xGridSize || currentFrame <= -xGridSize)
+            {
+                currentFrame = 0;
+            }
+
+            //apply frames
+            renderer.material.mainTextureOffset = new Vector2(vInput * ((currentFrame) % xGridSize + 1) * xGridPos, 1);
         }
+
+        //rotate
+        Quaternion rot = Quaternion.AngleAxis(hInput * playerRotSpeed * Time.deltaTime, transform.up) * transform.rotation;
+        transform.rotation = rot;
+        //move forward
+        transform.position += transform.forward * vInput * MoveSpeed() * Time.deltaTime;
+    }
+
+    //set speed according to zombie state
+    float MoveSpeed()
+    {
+        float moveSpeed = 0.0f;
+
+        switch (zombieStates)
+        {
+            case ZombieState.fullHuman:
+                moveSpeed = fullHumanSpeed;
+                break;
+            case ZombieState.partHuman:
+                moveSpeed = partHumanSpeed;
+                break;
+            case ZombieState.halfZombie:
+                moveSpeed = halfZombieSpeed;
+                break;
+            case ZombieState.nearFullZombie:
+                moveSpeed = nearFullZombieSpeed;
+                break;
+            case ZombieState.fullZombie:
+                moveSpeed = fullZombieSpeed;
+                break;
+        }
+
+        return moveSpeed;
     }
 }
