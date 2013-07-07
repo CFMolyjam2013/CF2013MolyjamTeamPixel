cbb5c39a34cf6f280162fbee0134499d1959468e
Matthew Wilber <m.wilber98@gmail.com>
2013-07-06 21:27:42 -0400

    Added the pickup sprites

--- Assets/Scripts/Objects/accuracyBuff.cs
+++ Assets/Scripts/Objects/accuracyBuff.cs
@@ -21,6 +21,9 @@ public class accuracyBuff : MonoBehaviour
     private float xGridPos = 0.0f;
     private float yGridPos = 0.0f;
 	
+	//Tempory time variable
+	private float timeVar = 0.0f;
+	
 	private PlayerPhysics playerPhysics;
 	
 	// Use this for initialization
@@ -32,6 +35,22 @@ public class accuracyBuff : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		//set grid tiles
+        xGridPos = 1.0f / xGridSize;
+        yGridPos = 1.0f;
+
+        //set scale
+        renderer.material.mainTextureScale = new Vector2(xGridPos, yGridPos);
+        
+		if((Mathf.Round(Time.time * 10)  % 2) == 0){
+			
+			nextTimeFrame = Time.time + frameDur;
+        
+			timeVar += 5 * Time.deltaTime;
+			currentFrame = (int)timeVar;
+			renderer.material.mainTextureOffset = new Vector2(((currentFrame) % xGridSize + 1) * xGridPos, 1);
+			
+        }
 		//Starts the powerup
 		if(startPowerUp == true)
 		{
